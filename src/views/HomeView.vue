0070bb28b8c0eb3a0c208a3dd657b7fddb2cc4b3
yousseframy24 <youssef.r@bronto24.com>
2023-12-02 23:45:20 +0200

    finishing intializing the project fully working with vue.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div className="w-full h-screen text-5xl flex justify-center items-center">
+      <h1>Welcome Sohila ;D</h1>
+    </div>
   </main>
 </template>
